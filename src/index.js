52146823484309c1cb499fbcafd777e50c49d7a7
nikola-sijakinjic <nikola@Nikolas-MacBook-Pro.local>
2017-08-03 16:23:00 +0200

    basic structure

--- src/index.js
+++ src/index.js
@@ -20,104 +20,56 @@ var unos = {
 		"zipcode": "10075",
 		"building": "1480",
 		"coord": [-73.9557413, 40.7720266]
-	},
-	"borough": "Manhattan",
-	"cuisine": "Italian",
-	"grades": [
-		{
-			"date": new Date("2014-10-01T00:00:00Z"),
-			"grade": "A",
-			"score": 11
-		},
-		{
-			"date": new Date("2014-01-16T00:00:00Z"),
-			"grade": "B",
-			"score": 17
-		}
-	],
-	"name": "Vella",
-	"restaurant_id": "41704620"
+	}
 };
 
+var async = require('async');
 
-// function insertDocument(db, entry) {
-// 	db.collection('restaurants').insertOne(entry).then(function (db) {
-// 		console.log("snimljeno");
-// 	}).catch(function (err) {
-// 		console.log("inserting failed ", err);
-// 	});
-// };
 
-// MongoClient.connect(url).then(function (db) {
-// 	console.log("go insert");
-// 	insertDocument(db, unos);
-// 	console.log("go inserted");
-// }).catch(function (err) {
-// 	console.log(err);
-// });
 
-// console.log(chai.request('http://localhost:8080')
-// 	.get('/v1/bpi/currentprice.json'));
-
-/*
-async function tryTransaction(url) {
-	rp(url)
-		.then(function (resp) {
-			console.log(resp);
-		})
-		.catch(function (err) {
-			console.log("Something went wrong");
-		});
-}
-var btcUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-var btcUrlRollback = 'https://jsonplaceholder.typicode.com/post/1'
-
-url = 'http://localhost:8000/zez.json'
-// tryTransaction(url);
-*/
-
-/*
-
-rp(url)
-	.then(function (htmlString) {
-		var recieved = JSON.parse(htmlString);
-		console.log(recieved);
-	})
-	.catch(function (err) {
-		console.log('none');
-	});
-*/
-
-async function tryAsync(url) {
-	try {
-		var ctx = [];
-		console.log("tryAsync called");
-
-		console.log('btc request passed');
-		var btcPrice = await rp(btcUrl);
-		ctx.push({ rollback: rp(btcUrlRollback) });
-		console.log(btcPrice);
-
-		var ethPrice = await rp(url);  // this one should fail
-		ctx.push({ rollback: function () { rb(btcUrlRollback) } });
-		console.log(ethPrice);
-
-	} catch (err) {
-		try {
-			while (ctx !== undefined && ctx.length != 0) {
-				console.log(ctx.pop().rollback());
-			}
-		} catch (error) {
-			console.log("failure during fail : ", ctx);
-		}
-		console.log("deviant : art");
+async.waterfall([
+	function (callback) {
+		callback(null, 'one', 'two');
+	},
+	function (arg1, arg2, callback) {
+		// arg1 now equals 'one' and arg2 now equals 'two'
+		callback(null, 'three');
+	},
+	function (arg1, callback) {
+		// arg1 now equals 'three'
+		callback(null, 'done');
 	}
+], function (err, result) {
+	// result now equals 'done'
+});
+
+// Or, with named functions:
+async.waterfall([
+	reserveFunds,
+	logToDatabase,
+	buyInsurance
+], function (err, ctx, result) {
+	console.log("err ", err);
+	console.log("ctx ", ctx);
+	console.log("result ", result);
+});
+
+
+function reserveFunds(callback) {
+	console.log('reserve')
+	ctx = [];
+	callback(null, ctx);
+}
+function logToDatabase(ctx, callback) {
+	console.log('logToDatabase')
+	callback(null, ctx);
+}
+function buyInsurance(ctx, callback) {
+	console.log('buyInsurance')
+	callback(null, ctx);
 }
 
 
-
-
-tryAsync("url");
 
 console.log("EXIT AT END");
 // process.exit(1);
