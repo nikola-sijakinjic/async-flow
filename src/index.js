b722667979bcbacb483054156b3315c93facf37e
nikola-sijakinjic <nikola@Nikolas-MacBook-Pro.local>
2017-08-04 10:46:14 +0200

    async-promises

--- src/index.js
+++ src/index.js
@@ -3,70 +3,40 @@ var Server = require('mongodb').Server;
 
 var assert = require('assert');
 var request = require('request');
-var chai = require('chai');
-var chaiHttp = require('chai-http');
 var rp = require('request-promise');
 
-chai.use(chaiHttp);
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+const bb = require('bluebird');
 
+var async = require('async');
 
-var mongoclient = new MongoClient(new Server("localhost", 27017), { native_parser: true });
-var request = require('request');
+var btc = require('./transactional/btcRequestRollback')
+async.waterfall([
+	function reserveFunds(callback) {
+		console.log("reserveFunds ");
+		rp(options)
+			.then(function (json) {
+				console.log("jsonString  --", json);
+				callback(null, json);
+			})
+			.catch(function (err) {
+				console.log("err  in reserver");
 
-var url = 'mongodb://localhost:27017/test';
-var unos = {
-	"address": {
-		"street": "2 Avenue",
-		"zipcode": "10075",
-		"building": "1480",
-		"coord": [-73.9557413, 40.7720266]
+				callback(err, null);
+			});
+	},
+	function (arg1, callback) {
+		console.log("err ");
+		callback(null, 'three');
+	},
+	function (arg1, callback) {
+		// arg1 now equals 'three'
+		callback(null, 'done');
 	}
-};
-
-var as = require('async');
-var btc = require('./transactional/btcRequestRollback');
-//var trouble = require('./transactional/problematicRequestRollback');
-
-
-
-var reserveFunds = async function () {
-	console.log("resere funds  : ");
-	var ctx = [];
-	return [ctx];
-}
-var logToDatabase = async function logToDatabase([ctx]) {
-	console.log("log to database context : ", ctx);
-	try {
-		var result = await btc.request();
-
-	} catch (err) {
-		ctx.push(btc.rollback);
-		console.log("not skipped : ", ctx);
-		throw { err: ctx };
-	}
-	return [ctx];
-}
-
-var buyInsurance = function buyInsurance([ctx]) {
-	console.log('buyInsurance context ', ctx);
-
-	return [ctx];
-}
-
-as.waterfall([
-	reserveFunds,
-	logToDatabase,
-	buyInsurance
-], function (err) {
-	console.log("FAIL -- ", err);
-
+], function (err, result) {
+	console.log("Failed");
 });
 
-
-
-console.log("EXIT AT END");
-// process.exit(1);
-
-
-
+// Or, with named functions:
 
--- src/transactional/btcRequestRollback.js
+++ src/transactional/btcRequestRollback.js
@@ -5,6 +5,47 @@ var rp = require('request-promise');
 var btcUrl = 'https://asdas'
 var btcUrlRollback = 'http://jsonplaceholder.typicode.com/posts/1'
 
+var options = {
+	uri: 'https://newton.now.sh/factor/21',
+	headers: {
+		'User-Agent': 'Request-Promise'
+	},
+	json: true // Automatically parses the JSON string in the response
+};
 
-module.exports.request = function () { return rp(btcUrl) };
-module.exports.rollback = function () { return rp(btcUrlRollback); };
+var optionsRollback = {
+	uri: 'https://newton.now.sh/factor/37',
+	headers: {
+		'User-Agent': 'Request-Promise'
+	},
+	json: true // Automatically parses the JSON string in the response
+};
+
+function btcRequest(options) {
+	return rp(options)
+		.then(function (json) {
+			console.log("jsonString  --", json);
+			callback(null, json);
+		})
+		.catch(function (err) {
+			console.log("err  in reserver");
+
+			callback(err, null);
+		});
+
+}
+
+function btcRollback(options) {
+	return rp(options)
+		.then(function (json) {
+			console.log("jsonString  --", json);
+			callback(null, json);
+		})
+		.catch(function (err) {
+			console.log("err  in reserver");
+
+			callback(err, null);
+		});
+}
+module.exports.request = btcRequest(options);
+module.exports.rollback = btcRollback(optionsRollback) };
