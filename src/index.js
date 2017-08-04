3597e276f4dd231357f5448976c5c830e62a02dd
nikola-sijakinjic <nikola@Nikolas-MacBook-Pro.local>
2017-08-04 19:18:43 +0200

    async promise radi

--- src/index.js
+++ src/index.js
@@ -11,26 +11,68 @@ const bb = require('bluebird');
 
 var async = require('async');
 
-var btc = require('./transactional/btcRequestRollback')
-var problem = require('./transactional/problematicRequestRollback.js')
+// var btc = require('./transactional/btcRequestRollback')
+// var problem = require('./transactional/problematicRequestRollback.js')
+
 
 async.waterfall([
-	function reserveFunds(callback) {
-		console.log("reserveFunds");
-		btc.request(callback)
+	function reserveFunds(waterfall) {
+		ctx = [];
+		rp('https://newton.now.sh/factor/21')
+			.then(function (json) {
+				console.log("reserver succeded", json);
+
+				// rolback if later operation fails
+				ctx.push(function () {
+					rp('https://newton.now.sh/factor/121')
+						.then(function (htmlString) {
+							console.log("reserve rollback executed");
+						})
+						.catch(function (err) {
+							console.log("FATAL");
+						})
+				});
+				waterfall(null, ctx);
+			})
+			.catch(function (err) {
+				waterfall(err, ctx);
+			});
 	},
-	function saveLogToDatabase(arg1, callback) {
-		console.log("seccond method");
-		callback(null, 'three');
+	function saveLogToDatabase(ctx, waterfall) {
+		console.log("say what");
+		waterfall(null, ctx);
 	},
-	function (arg1, callback) {
-		console.log("third method");
-		problem.request(callback);
+	function (ctx, waterfall) {
+		rp('xhttps://newton.now.sh/factor/1')
+			.then(function (json) {
+				console.log("reserver succeded", json);
+
+				// rolback if later operation fails
+				ctx.push(function () {
+					rp('https://newton.now.sh/factor/2')
+						.then(function (htmlString) {
+							console.log("reserve rollback executed");
+						})
+						.catch(function (err) {
+							console.log("FATAL");
+						})
+				});
+				waterfall(null, ctx);
+			})
+			.catch(function (err) {
+				waterfall(err, ctx);
+			});
+
 	}
-], function (err, result) {
+], function (err, ctx) {
 	if (err != null) {
-		console.log("Failed");
+		while (ctx.length > 0) {
+			console.log("loop");
+			rollback = ctx.pop();
+			rollback();
+		}
 	} else {
+
 		console.log("Success");
 	};
 });
